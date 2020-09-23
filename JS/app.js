5ee8b3510ecb44fc70160d288ceb1c3102fd307e
Ervin MG <ervinjoel2323@gmail.com>
2020-09-23 10:49:57 -0600

    probando funciones

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a+b;   //Funcion de suma que devuelve una suma.
+}
+
+const total = sum(10, 200)  //declaramos que lso valores de la funcion suma tendra estos dos valores.
+
+console.log("EL total es: ",total);
