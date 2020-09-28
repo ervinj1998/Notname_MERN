21f32eb287118eb14db18c332a8a516618dfc503
Ervin MG <ervinjoel2323@gmail.com>
2020-09-27 23:43:00 -0600

    primeros pasos api

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,13 +1,17 @@
-const express = require ('express')
-const app = express()
+const express = require('express')
+const app = express();
+const morgan = require('morgan');
 
-app.get("/", (req, res) => {
-    res.send("Hello world from node js.");
+// bring in routes
+
+const {
+    getPosts
+} = require("./routes/post");
+
+app.get("/", getPosts);
+
+const port = 8081;
+app.listen(port, () => {
+    console.log(`A node Js API listening on port: ${port}`);
 
 });
-
-const port = 8080;
-app.listen(port, () => {console.log(`A node Js API listening on port: ${port}`);
-
-});
-
